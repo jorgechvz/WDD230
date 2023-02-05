e6661f33d31dd245bc4bc83fb7356a5ec2d6291a
jorgechvz <cha22060@byui.edu>
2023-02-04 23:26:24 -0500

    Add news features in the home page

--- chamber/scripts/app.js
+++ chamber/scripts/app.js
@@ -1,6 +1,6 @@
 /* Function for time */
-time = function (decision){
-    const time = new Date();
+const time = new Date();
+const functionTime = function (decision){
     let weekDays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     if (decision === "headerTime"){
@@ -18,7 +18,30 @@ function toggleMenu(){
 /* DOM */
 const x = document.getElementById("hamburgerBtn");
 x.onclick = toggleMenu;
-document.querySelector(".header_time").innerHTML = time("headerTime");
-document.querySelector(".copyright_year").innerHTML = time("copyright");
+document.querySelector(".header_time").innerHTML = functionTime("headerTime");
+document.querySelector(".copyright_year").innerHTML = functionTime("copyright");
 document.querySelector(".last_updated").innerHTML = new Date(document.lastModified).toLocaleString("en-UK");
 
+/* For the display Event only monday and tuesday */
+
+const eventSection = document.querySelector(".event_section");
+const mainSection = document.querySelector("main");
+const weatherSection = document.querySelector(".weather_section");
+const heroSection = document.querySelector(".hero_section");
+const newsSection = document.querySelector(".news_section");
+const spotlightSection = document.querySelector(".spotlight_section");
+const joinSection = document.querySelector(".join_section");
+const imageJoin = document.querySelector(".join_image");
+const currentDay = new Date().getDay();
+
+if (currentDay !== 1 && currentDay !== 2) {
+    eventSection.style.display = "none";
+} else{
+    weatherSection.classList.add('weather_section-grid');
+    heroSection.classList.add('hero_section-grid');
+    newsSection.classList.add('news_section-grid');
+    spotlightSection.classList.add('spotlight_section-grid');
+    joinSection.classList.add('join_section-grid');
+    imageJoin.classList.add('image_section-grid');
+    mainSection.classList.add('main-grid');
+}
